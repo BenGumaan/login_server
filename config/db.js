b3007e688d8c5a62525e043056f34c3d8a9fafcf
Ben Gumaan <ben.gumaan@gmail.com>
2023-11-02 10:08:36 +0100

    Commit after fixing server issues

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
+// console.log("TEST: ",require('dotenv').config())
 const mongoose = require('mongoose');
-
+// console.log("DB", process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("DB Connected");
 }).catch((err) => console.log(err));
